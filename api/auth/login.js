7221e83b4e3498a4b4154117520826c3001fc783
DiannaStamper <diannastamper@gmail.com>
2026-05-14 15:25:52 -0400

    Update login.js

--- api/auth/login.js
+++ api/auth/login.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   const clientId = process.env.MEMBERFUL_CLIENT_ID;
   const redirectUri = encodeURIComponent('https://dashboard.myRealLifeMoney.com/api/auth/callback');
-  const authUrl = `https://myreallifemoney.memberful.com/oauth?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}`;
+  const authUrl = `https://myreallifemoney.memberful.com/oauth?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=read`;
   return res.redirect(authUrl);
 }
